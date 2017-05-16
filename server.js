924e37337940e4e2aedf54ea552e64d89dcf7c59
marcelo10 <marcelo79@gmail.com>
2017-05-15 20:52:04 -0500

    events refined, log enhaced, duplication removed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss.l');
+const util = require('util')
 
 var Buffer = require('buffer').Buffer;
 var dgram = require('dgram');
@@ -23,22 +24,44 @@ wss.on('connection', function(ws) {
 
     //When a message is received from udp server send it to the ws client
     udpServer.on('message', function(msg, rinfo) {
-        console.log("UDP TO WS -> " + msg.toString());
-       ws.send(msg.toString(),function(error){
-         if(error != null) 
-            {
-                console.log('UDP_TO_WS: SOCKET ERROR %s', error);
-                //ws.close();
+    
+    //console.log("[MIDDLEWARE][SRC "+rinfo.address+"]");
+
+    ws.send(msg.toString(),function(error){
+        if(error != null) {
+            //console.log('[MIDDLEWARE] UDP_TO_WS: SOCKET ERROR %s', error);
+            //ws.close();
+        } else {
+            var wsPayload = JSON.parse(msg.toString());
+            var type = wsPayload.name;
+            var state = wsPayload.val;
+
+            if(type=='moving' && state==true) {                
+                console.log("[MIDDLEWARE] MOVEMENT STARTED => " + msg.toString());
+            } else if(type=='moving' && state==false) {                
+                console.log("[MIDDLEWARE] MOVEMENT STOPPED => " + msg.toString());
+            } else if (type=='magnet' && state==true){
+                console.log("[MIDDLEWARE] MAGNET DETECTED => " + msg.toString());
+            } else if (type=='magnet' && state==false){
+                console.log("[MIDDLEWARE] MAGNET GONE => " + msg.toString());
+            }
          }
-    });
+        });
     });
  
     //When a message is received from ws client send it to udp server.
     ws.on('message', function(message) {
         var msgBuff = new Buffer(message);
-        console.log("WS TO UDP -> " + msgBuff);
+
+        if(msgBuff=='G') {
+            console.log("[MIDDLEWARE] GO SIGNAL RECEIVED");
+        } else if(msgBuff=='S') {
+            console.log("[MIDDLEWARE] STOP SIGNAL RECEIVED");
+        }
+
         udpClient.send(msgBuff, 0, msgBuff.length, SERVER_FWD_PORT, SERVER_FWD_IP);
     });
+    ws.close;
 });
 
 udpServer.bind(SERVER_PORT, SERVER_IP);
